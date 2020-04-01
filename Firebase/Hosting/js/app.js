216dd63478efda369be709e8cab12b7f4dfe58b7
Matt Reid <hello@mattreid.dev>
2020-04-01 12:21:33 +0100

    Added Firebase and renamed login page.

---
+++ Firebase/Hosting/js/app.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+	apiKey: "AIzaSyCELqGd1KY8mtLbBLuaWGR-n8Cv6x1SeCQ",
+	authDomain: "com2027-group-20.firebaseapp.com",
+	databaseURL: "https://com2027-group-20.firebaseio.com",
+	projectId: "com2027-group-20",
+	storageBucket: "com2027-group-20.appspot.com",
+	messagingSenderId: "260658215959",
+	appId: "1:260658215959:web:0d16d2c9800d71397e3052",
+	measurementId: "G-LSNE703MJ1"
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
