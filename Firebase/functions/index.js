97327cef46b13545e9fd93b22b1f21941aee654c
Camran <puddl3s@github.com>
2020-04-15 16:00:49 +0200

    Initial createUserAccount function attempt (broken)

--- Firebase/functions/index.js
+++ Firebase/functions/index.js
@@ -1,4 +1,17 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions')
+const admin = require('firebase-admin')
+admin.initializeApp(functions.config().firebase);
+
+const ref = admin.database().ref()
+
+exports.createUserAccount = functions.auth.user().onCreate(event => {
+    const uid = event.data.uid
+    const email = event.data.email
+    const newUserRef = ref.child('/users/${uid}')
+    return newUserRef.set({
+        email: email
+    })
+})
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
