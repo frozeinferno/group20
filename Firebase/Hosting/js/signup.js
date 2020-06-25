6076c8682fdb6242694289969f227462e367958d
Camran <puddl3s@github.com>
2020-06-25 15:19:53 +0100

    adjusted db entry to incorperate new fields in the registration

--- Firebase/Hosting/js/signup.js
+++ Firebase/Hosting/js/signup.js
@@ -6,14 +6,19 @@ signupForm.addEventListener('submit', (e) => {
     // get user info
     const email = signupForm['email'].value;
     const password = signupForm['password'].value;
+    const dob = signupForm['dob'].value;
+    const gender = signupForm['gender'].value;
+    const interests = signupForm['interests'].value;
+    
   
-    console.log(email, password);
+    console.log(email, password, dob, gender, interests);
 
     auth.createUserWithEmailAndPassword(email, password).then(cred => {
         console.log(cred.user);
         return db.collection('users').doc(cred.user.uid).collection('user_writeable').doc('profile').set({
-            gender: 'female',
-            interests: 'volleyball'
+            dob: dob,
+            gender: gender,
+            interests: interests
         });
 
     });
