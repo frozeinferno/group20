8f7e6c944062502c15c9dc0ae46540ecba726c6f
Camran <puddl3s@github.com>
2020-06-27 18:03:46 +0100

    Added commenting and commented out debugging code

--- Firebase/Hosting/js/signup.js
+++ Firebase/Hosting/js/signup.js
@@ -3,27 +3,31 @@ const signupForm = document.querySelector('#signup-form');
 signupForm.addEventListener('submit', (e) => {
     e.preventDefault();
     
-    // get user info
+    // Retrieve user info from the sign up form
+    // ToDo - validation on fields
     const email = signupForm['email'].value;
     const password = signupForm['password'].value;
     const dob = signupForm['dob'].value;
     const gender = signupForm['gender'].value;
     const interests = signupForm['interests'].value;
     
-  
-    console.log(email, password, dob, gender, interests);
+    // Temporary console logging for debugging
+    //console.log(email, password, dob, gender, interests);
 
+    // Creating an authorized user with email and password
     auth.createUserWithEmailAndPassword(email, password).then(cred => {
-        console.log(cred.user);
+        console.log(cred.user.uid);
+        // Creating a Firestore entry with the users id attaching the relevant fields of the sign up form
         return db.collection('users').doc(cred.user.uid).collection('user_writeable').doc('profile').set({
             dob: dob,
             gender: gender,
             interests: interests
         });
+    // An error catch to alert the user about a failed sign up
     }).catch(function(error) {
         var errorCode = error.code;
         var errorMessage = error.message;
         console.log(errorMessage);
-        return null;
+        return null; 
     });
 });
