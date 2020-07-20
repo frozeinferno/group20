2e294483990331b83a9ea08c6936885f0252fa40
Seif <seifhishamtaha@gmail.com>
2020-07-20 19:06:00 +0200

    fb login no db

--- Firebase/Hosting/js/login.js
+++ Firebase/Hosting/js/login.js
@@ -22,3 +22,33 @@ loginForm.addEventListener('submit', (e) => {
         return null; 
     });
 });
+function fbsignin(){
+    var provider = new firebase.auth.FacebookAuthProvider();
+    provider.addScope('user_birthday,email');
+    firebase.auth().useDeviceLanguage();
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+  // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+  var token = result.credential.accessToken;
+  // The signed-in user info.
+  console.log(user);
+  var user = result.user;
+  
+  return db.collection('users').doc(user.uid).collection('user_writeable').doc('profile').set({
+    dob: 'user_birthday'
+}).then(cred => {
+    window.location.href = "/event.html";
+});
+  // ...
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // ...
+});
+
+
+    }
