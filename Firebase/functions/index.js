12fa1976a015a2abcd95190a5e2e93927a5c7ac8
Matt Reid <hello@mattreid.dev>
2020-08-14 13:47:57 +0100

    Refactored and added todo to Cloud Functions.

--- Firebase/functions/index.js
+++ Firebase/functions/index.js
@@ -1,27 +1,27 @@
-const functions = require('firebase-functions')
-const admin = require('firebase-admin')
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+
 admin.initializeApp(functions.config().firebase);
+const db = admin.firestore();
 
-const db = admin.firestore()
+exports.createUserAccount = functions.region("europe-west1").auth.user().onCreate((event) => {
+	const uid = event.uid;
+	const newUserRef = db.collection("users").doc(`${uid}`);
+	return newUserRef.set({
+		uid: uid
+	}).then(function () {
+		console.log("Document successfully created!");
+	}).catch(function (error) {
+		console.error("Error creating document: ", error);
+	});
+});
 
-exports.createUserAccount = functions.region('europe-west1').auth.user().onCreate(event => {
-    const uid = event.uid
-    const newUserRef = db.collection("users").doc(`${uid}`)
-    return newUserRef.set({
-        uid: uid
-    }).then(function() {
-        console.log("Document successfully created!")
-    }).catch(function(error) {
-        console.error("Error creating document: ", error);
-    })
-})
-
-exports.deleteUserAccount = functions.region('europe-west1').auth.user().onDelete(event => {
-    const uid = event.uid
-    const userRef = db.collection("users").doc(`${uid}`)
-    return userRef.delete().then(function() {
-        console.log("Document successfully deleted!")
-    }).catch(function(error) {
-        console.error("Error removing document: ", error);
-    })
-})
+exports.deleteUserAccount = functions.region("europe-west1").auth.user().onDelete((event) => {
+	const uid = event.uid;
+	const userRef = db.collection("users").doc(`${uid}`);
+	return userRef.delete().then(function () {
+		console.log("Document successfully deleted!"); // TODO: Remove orphaned events
+	}).catch(function (error) {
+		console.error("Error removing document: ", error);
+	});
+});
